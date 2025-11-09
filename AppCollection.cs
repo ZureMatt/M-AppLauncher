4558be93db869e05f209231915f82e3c668dbaaf
ZureMatt <matthieukamphuis44@gmail.com>
2025-11-09 14:13:03 +0100

    Add project files.

---
+++ AppCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_AppLauncher
+{
+    public class AppCollection
+    {
+        public int Id { get; set; }
+        public string AppName { get; set; }
+        public string ExePath { get; set; }
+
+    }
+}
