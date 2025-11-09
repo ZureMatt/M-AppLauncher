0ba2c87606839d0acadfeccd89466b32a3747948
ZureMatt <matthieukamphuis44@gmail.com>
2025-11-09 14:50:29 +0100

    Renamed and organized the file structure. Made sure that App is its own model and there's an AppCollectionHandler that includes a List<App> and can manipulate that List<App> however desired.

---
+++ Handlers/AppCollectionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace M_AppLauncher
+{
+    public class AppCollectionHandler
+    {
+        List<App> AppCollection;
+        string JsonPath = "";
+
+        public List<App> GetAppCollection()
+        {
+            //Retrieve AppCollection from JSON
+            AppCollection = JsonSerializer.Deserialize<List<App>>(JsonPath);
+
+            return AppCollection;
+        }
+    }
+}
--- Models/App.cs
+++ Models/App.cs
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace M_AppLauncher
 {
-    public class AppCollection
+    public class App
     {
         public int Id { get; set; }
         public string AppName { get; set; }
         public string ExePath { get; set; }
-
     }
 }
